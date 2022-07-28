347bcc73f922555cf4d90d09a7340b935d5bd575
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-28 02:26:16 -0500

    Complete

--- main.py
+++ main.py
@@ -40,6 +40,38 @@ def hang_man():
         print(f"Word:{' '.join(word_list)}")
         
         word_user = input("Choose the word: ").upper()
+    """
+        if the word chosen by the user is in the alphabet and is not in the set of letters that have been entered, 
+        the letter is added to the set of letters entered
+    """
         
-        if word_user in 
+    if word_user in alphabet - guessed_letters:
+        guessed_letters.add(word_user)
     # 'Python'= {'P', 'y', 't','h', 'o', 'n'}
+    
+    # If the letter is in the word
+        if word_user in guessed_letters:
+            guessed_letters.remove(word_user)
+            print('')
+        else:
+            lifes = lifes - 1
+            print(f"\nYou letter, {word_user} it's not your word.")
+    # If the letter chosen of the user was already entered
+
+    elif word_user in guessed_letters:
+        print("\nYou already picked that letter. Please choose a new letter.")
+    else:
+        print("\nThis letter is not valid.")
+
+    """
+    The game reaches this line when all the letters of the word 
+    are guessed or when the player's lives are exhausted.
+    """
+    if lifes == 0:
+        print(lifes_dictionary_visual[lifes])
+        print(f"!Hanged¡ you lost. So sorry. The word was: {word} ")
+    else:
+        print(f"!Excellent¡ you guessed the word {word}")
+
+if __name__ == '__main__':    
+    hang_man()
